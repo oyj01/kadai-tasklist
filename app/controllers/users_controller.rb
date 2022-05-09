2e0cadff8bbef2436016e8557ff4b6911ea612ee
oyj01 <1995ouyangjun@gmail.com>
2022-05-09 07:03:47 +0000

    update 5/9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   
   before_action :require_user_logged_in, only: [:index, :show]
   
-  def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 25)
-  end
+  # def index
+  #   @pagy, @users = pagy(User.order(id: :desc), items: 25)
+  # end
 
-  def show
-    @user = User.find(params[:id])
-    @pagy, @tasks = pagy(@user.tasks.order(id: :desc))
-    counts(@user)
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  #   @pagy, @tasks = pagy(@user.tasks.order(id: :desc))
+  #   counts(@user)
+  # end
 
   def new
     @user = User.new
